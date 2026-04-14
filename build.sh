794e088d0aede97ae7c573d9f06cfc7da052d0ee
Gianpedrobc <devgianpedro@gmail.com>
2026-04-13 21:18:11 -0300

    fix build.sh permission

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@email.com', 'admin123')
+EOF
